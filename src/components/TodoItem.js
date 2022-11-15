ad01911c57dd2fe5ae53b2ac1b444e1863e4466d
LEAP\Gabriel.McHugh <gabriel.t.mchugh@gmail.com>
2022-11-15 15:59:51 +1000

    added inline styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,12 @@
 import React from "react"
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
   render() {
     return (
-      <li>
+      <li className={styles.item}>
         <input
+          className={styles.checkbox}
           type="checkbox"
           checked={this.props.todo.completed}
           onChange={() => this.props.handleChangeProps(this.props.todo.id)}
